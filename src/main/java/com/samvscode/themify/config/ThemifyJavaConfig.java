60f134eaf7cbbb9c0e9f1092e046165d47e0bff4
Sanyam Singh <ssingh18@atlassian.com>
2024-05-01 12:49:23 +0530

    creating a velocity template for themify react servlet

--- src/main/java/com/samvscode/themify/config/ThemifyJavaConfig.java
+++ src/main/java/com/samvscode/themify/config/ThemifyJavaConfig.java
@@ -5,6 +5,7 @@ import com.samvscode.themify.impl.ThemifyComponentImpl;
 import com.atlassian.plugins.osgi.javaconfig.configs.beans.ModuleFactoryBean;
 import com.atlassian.plugins.osgi.javaconfig.configs.beans.PluginAccessorBean;
 import com.atlassian.sal.api.ApplicationProperties;
+import com.atlassian.templaterenderer.TemplateRenderer;
 
 import org.osgi.framework.ServiceRegistration;
 import org.springframework.beans.factory.FactoryBean;
@@ -32,6 +33,11 @@ public class ThemifyJavaConfig {
     @Bean
     public ThemifyComponent themifyComponent(ApplicationProperties applicationProperties) {
         return new ThemifyComponentImpl(applicationProperties);
+    }
+
+    @Bean
+    public TemplateRenderer templateRenderer(){
+        return importOsgiService(TemplateRenderer.class);
     }
 
     // Exports ThemifyComponent as an OSGi service
--- src/main/java/com/samvscode/themify/servlet/ThemifyReactServlet.java
+++ src/main/java/com/samvscode/themify/servlet/ThemifyReactServlet.java
@@ -3,6 +3,8 @@ package com.samvscode.themify.servlet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.atlassian.templaterenderer.TemplateRenderer;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,12 +13,18 @@ import java.io.IOException;
 
 public class ThemifyReactServlet extends HttpServlet{
     private static final Logger log = LoggerFactory.getLogger(ThemifyReactServlet.class);
+    
+    private final TemplateRenderer renderer;
+
+    public ThemifyReactServlet(TemplateRenderer renderer){
+        this.renderer = renderer;
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
         resp.setContentType("text/html");
-        resp.getWriter().write("<html><body>Hello World</body></html>");
+        renderer.render("templates/themify-settings.vm", resp.getWriter());
     }
 
 }
